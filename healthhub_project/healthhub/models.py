0fc9667deea4995817d3385890ddb2a804bb3ac6
kobusszymon <szymon.kobus@icloud.com>
2025-11-13 19:08:16 +0100

    Add model with users

--- healthhub_project/healthhub/models.py
+++ healthhub_project/healthhub/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Plec(models.IntegerChoices):
+        MEZCZYZNA = 1, "Mężczyzna"
+        KOBIETA = 2, "Kobieta"
+        INNA = 3, "Inna"
+
+class Uzytkownik(models.Model):
+        imie = models.CharField(max_length = 50)
+        nazwisko = models.CharField(max_length = 100)
+        plec = models.IntegerField(choices = Plec.choices, default = 3)
+        telefon = models.CharField(max_length = 9, blank = True)
+        email = models.EmailField
+        data_utworzenia = models.DateTimeField (auto_now_add = True, editable = False)
