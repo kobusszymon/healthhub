b08153e70cf336413327c56cb7316bb35f265630
kobusszymon <szymon.kobus@icloud.com>
2026-01-22 17:33:52 +0100

    Add pomiar to REST framework

--- healthhub_project/healthhub/urls.py
+++ healthhub_project/healthhub/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("profile/", views.profil_list, name="profil-list"),
-    path("profile/<int:pk>/", views.profil_detail, name="profil-detail"),
+    path('profile/', views.profil_list, name = 'profil-list'),
+    path('profile/<int:pk>/', views.profil_detail, name = 'profil-detail'),
+    path('pomiar/', views.pomiar_list, name ='pomiar-list'),
+    path('pomiar/<int:pk>/', views.pomiar_detail, name = 'pomiar-detail'),
 ]
--- healthhub_project/healthhub/views.py
+++ healthhub_project/healthhub/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import ProfilUzytkownika
-from .serializers import ProfilUzytkownikaSerializer
+from .models import ProfilUzytkownika, Pomiar, Lek
+from .serializers import ProfilUzytkownikaSerializer, PomiarSerializer, LekSerializer
 
 @api_view(['GET', 'POST'])
 def profil_list(request):
     if request.method == 'GET':
-        profiles = ProfilUzytkownika.objects.all()
-        serializer = ProfilUzytkownikaSerializer(profiles, many=True)
+        profile = ProfilUzytkownika.objects.all()
+        serializer = ProfilUzytkownikaSerializer(profile, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     elif request.method == 'POST':
@@ -19,25 +19,60 @@ def profil_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
 @api_view(['GET', 'PUT', 'DELETE'])
 def profil_detail(request, pk):
     try:
-        profile = ProfilUzytkownika.objects.get(pk=pk)
+        profil = ProfilUzytkownika.objects.get(pk=pk)
     except ProfilUzytkownika.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = ProfilUzytkownikaSerializer(profile)
+        serializer = ProfilUzytkownikaSerializer(profil)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
-        serializer = ProfilUzytkownikaSerializer(profile, data=request.data)
+        serializer = ProfilUzytkownikaSerializer(profil, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
-        profile.delete()
+        profil.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def pomiar_list(request):
+    if request.method == 'GET':
+        pomiary = Pomiar.objects.all()
+        serializer = PomiarSerializer(pomiary, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = PomiarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def pomiar_detail(request, pk):
+    try:
+        pomiar = Pomiar.objects.get(pk=pk)
+    except Pomiar.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = PomiarSerializer(pomiar)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'PUT':
+        serializer = PomiarSerializer(pomiar, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        pomiar.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
