424c69891aeeaa24f568b8f1190f995be77c3dbf
kobusszymon <szymon.kobus@icloud.com>
2026-01-28 20:57:00 +0100

    Add permissions to detail views and new views with query sets

--- healthhub_project/healthhub/urls.py
+++ healthhub_project/healthhub/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('profil/<int:pk>/', views.profil_detail, name = 'profil-detail'),
     path('pomiar/', views.pomiar_list, name ='pomiar-list'),
     path('pomiar/<int:pk>/', views.pomiar_detail, name = 'pomiar-detail'),
+    path('pomiar/uzytkownik', views.pomiar_uzytkownika, name ='pomiar-uzytkownika'),
     path('lek/', views.lek_list, name ='lek-list'),
     path('lek/<int:pk>/', views.lek_detail, name = 'lek-detail'),
     path('aktywnosc/', views.aktywnosc_list, name ='aktywnosc-list'),
@@ -15,5 +16,9 @@ urlpatterns = [
     path('lokalizacja/<int:pk>/', views.lokalizacja_detail, name = 'lokalizacja-detail'),
     path('lokalizacja/update_delete/<int:pk>/', views.lokalizacja_update_delete, name = 'lokalizacja_update_delete'),
     path('termin/', views.termin_list_admin, name ='termin-list'),
+    path('termin/przyszle', views.przyszle_wizyty_uzytkownika, name = 'przyszle-wizyty-uzytkownka'),
+    path('termin/wolne', views.wolne_terminy, name = 'wolne-terminy'),
+    path('termij/wolne/specjalizacja', views.wolne_terminy_specjalizacja),
     path('termin/<int:pk>/', views.termin_detail, name = 'termin-detail'),
+    path('termin/<int:pk>/rezerwuj', views.rezerwuj_termin, name = 'rezerwuj-termin'),
 ]
--- healthhub_project/healthhub/views.py
+++ healthhub_project/healthhub/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.decorators import permission_required
+from django.utils import timezone
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser
 from .permission import IsUzytkownik
 from .models import ProfilUzytkownika, Pomiar, Lek, Aktywnosc, Lokalizacja, TerminWizyty
 from .serializers import ProfilUzytkownikaSerializer, PomiarSerializer, LekSerializer, AktywnoscSerializer, LokalizacjaSerializer, TerminWizytySerializer
@@ -71,8 +72,25 @@ def pomiar_list(request):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    
+
+@api_view(['GET', 'POST'])
+def pomiar_uzytkownika(request):
+    if request.method == 'GET':
+        pomiary = Pomiar.objects.filter(uzytkownik=request.user)
+        serializer = PomiarSerializer(pomiary, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = PomiarSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(uzytkownik = request.user)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminUser])
 def pomiar_detail(request, pk):
     try:
         pomiar = Pomiar.objects.get(pk = pk)
@@ -118,16 +136,31 @@ def lek_detail(request, pk):
     
     if request.method == 'GET':
         serializer = LekSerializer(lek)
+        if not (
+            IsUzytkownik().has_object_permission(request, None, lek)
+            or IsAdminUser().has_permission(request, None)
+        ):
+            return Response(status=status.HTTP_403_FORBIDDEN)
         return Response(serializer.data, status = status.HTTP_200_OK)
     
     if request.method == 'PUT':
         serializer = LekSerializer(lek, data = request.data)
+        if not (
+            IsUzytkownik().has_object_permission(request, None, lek)
+            or IsAdminUser().has_permission(request, None)
+        ):
+            return Response(status=status.HTTP_403_FORBIDDEN)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
+        if not (
+            IsUzytkownik().has_object_permission(request, None, lek)
+            or IsAdminUser().has_permission(request, None)
+        ):
+            return Response(status=status.HTTP_403_FORBIDDEN)
         lek.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
     
@@ -147,6 +180,7 @@ def aktywnosc_list(request):
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminUser])
 def aktywnosc_detail(request, pk):
     try:
         aktywnosc = Aktywnosc.objects.get(pk = pk)
@@ -167,6 +201,20 @@ def aktywnosc_detail(request, pk):
     elif request.method == 'DELETE':
         aktywnosc.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def aktywnosc_uzytkownika(request):
+    if request.method == 'GET':
+        aktywnosci = Aktywnosc.objects.filter(uzytkownik=request.user)
+        serializer = AktywnoscSerializer(aktywnosci, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = AktywnoscSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(uzytkownik = request.user)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -229,7 +277,31 @@ def termin_list_admin(request):
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET'])
+def wolne_terminy(request):
+    if request.method == 'GET':
+        terminy = TerminWizyty.objects.filter(uzytkownik__isnull = True, data_wizyty__gte = timezone.now())
+        serializer = TerminWizytySerializer(terminy, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+@api_view(['GET'])
+def wolne_terminy_specjalizacja(request):
+    if request.method == 'GET':
+        specjalizacja = request.query_params.get('specialty', None)
+        terminy = TerminWizyty.objects.filter(uzytkownik__isnull = True, data_wizyty__gte = timezone.now(), specjalizacja__icontains = specjalizacja)
+        serializer = TerminWizytySerializer(terminy, many = True)
+        if specializacja is not None:
+            return Response(serializer.data, status = status.HTTP_200_OK)
+
+@api_view(['GET'])
+def przyszle_wizyty_uzytkownika(request):
+    if request.method == 'GET':
+        terminy = TerminWizyty.objects.filter(uzytkownik__isnull = True, data_wizyty__gte = timezone.now())
+        serializer = TerminWizytySerializer(terminy, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
 @api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAdminUser])
 def termin_detail(request, pk):
     try:
         termin = TerminWizyty.objects.get(pk = pk)
@@ -250,3 +322,20 @@ def termin_detail(request, pk):
     elif request.method == 'DELETE':
         termin.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+@api_view(['PUT'])
+def rezerwuj_termin(request, pk):
+    try:
+        termin = TerminWizyty.objects.get(pk = pk)
+    except TerminWizyty.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        if termin.uzytkownik is not None:
+            return Response({"detail": "Ten termin jest już zajęty przez inną osobę."},
+            status=status.HTTP_400_BAD_REQUEST)
+        serializer = TerminWizytySerializer(termin, data = request.data)
+        if serializer.is_valid():
+            serializer.save(uzytkownik=request.user)
+            return Response(serializer.data, status = status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
